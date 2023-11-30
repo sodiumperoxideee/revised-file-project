6078d414661a6dd0f25ff03bff1441e20d8fce0f
Sunshine Casido <qb202100057@wmsu.edu.ph>
2023-12-01 07:48:41 +0800

    ayaw mag-add

--- admin/addfacilities.php
+++ admin/addfacilities.php
@@ -3,19 +3,12 @@
     require_once '../classes/facilities.class.php';
     require_once  '../tools/functions.php';
 
-    //resume session here to fetch session values
     session_start();
-    /*
-        if user is not login then redirect to login page,
-        this is to prevent users from accessing pages that requires
-        authentication such as the dashboard
-    */
+    
     if (!isset($_SESSION['user']) || $_SESSION['user'] != 'employee'){
         header('location: ./index.php');
     }
     
-    //if the above code is false then html below will be displayed
-
     if(isset($_POST['save'])){
 
         $facilities = new Facilities();
@@ -40,6 +33,7 @@
         }
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
--- classes/account.class.php
+++ classes/account.class.php
@@ -33,5 +33,22 @@ class Account{
     }    
 
     
+    function sign_in_staff(){
+        $sql = "SELECT * FROM staff WHERE email = :email LIMIT 1;";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        
+        if ($query->execute()) {
+            $accountData = $query->fetch(PDO::FETCH_ASSOC);
+    
+            if ($accountData && password_verify($this->password, $accountData['password'])) {
+                $this->user_id = $accountData['user_id'];
+                return true;
+            }
+        }
+    
+        return false;
+    }    
+
 }
 ?>
