e09de349677fe966b4a06ae4a119541ec59f5479
chlorioineychee <makseyndelacruz@gmail.com>
2023-12-01 00:33:46 +0800

    added footer.php

--- user/emergency-care.php
+++ user/emergency-care.php
@@ -161,37 +161,8 @@
       </div>   
   </main>
 
-  <footer class="mt-5 pt-2">
-    <div class="row pt-4">
-      <div class="col one mx-5 pe-5">
-        <a href="index.html"><h1 class="">Purrpaws</h1></a>
-        <h4 class="">A friendly vet clinic that cares for the health and happiness of your cats and dogs.</h4>
-        <div class="socmed py-1">
-          <a href="#"><i class="fa-brands fa-facebook me-2" style="color: #ff2635;"></i></a>
-          <a href="#"><i class="fa-brands fa-instagram me-2" style="color: #ff2635;"></i></a>
-          <a href="#"><i class="fa-brands fa-linkedin me-2" style="color: #ff2635;"></i></a>
-          <a href="#"><i class="fa-brands fa-youtube me-2" style="color: #ff2635;"></i></a>
-        </div>
-      </div>
-      <div class="col two my-1 mx-5 pe-5">
-        <h5>Clinic Information</h5>
-        <p class="my-1 ms-2">185 Don Alfaro St, Zamboanga City, Zamboanga del Sur</p>
-        <p class="my-1 ms-2">(+63) 927 5150 836</p>
-        <p class="my-1 ms-2">purrpawsvetclinic@gmail.com</p>
-      </div>
-      <div class="col three my-1 mx-5"> 
-        <h5>Hours of Operation</h5>
-        <p class="my-1 ms-2">Tuesday to Friday: 10:00 AM - 7:00 PM</p>
-        <p class="my-1 ms-2">Saturday: 9:00 AM - 3:00 PM</p>
-        <p class="my-1 ms-2">Sunday and Monday: Closed</p>
-      </div>
-    </div>
-    <div class="uh d-flex justify-content-center">
-        <p class="my-3">For after-hour emergencies, please call <a href="tel:+63275150836">+63 927 5150 836</a>.</p>
-    </div>
-    <div class="col d-flex justify-content-center">
-              <p>&copy; 2023 Purrpaws. All rights reserved.</p>
-          </div>
-  </footer>
+  <?php
+    require_once('include/user-footer.php')
+  ?>
 </body>
 </html>
