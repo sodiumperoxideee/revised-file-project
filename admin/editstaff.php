085ad7042fbd102484d5758ef8189dd79ff7dbb4
Sunshine Casido <qb202100057@wmsu.edu.ph>
2023-12-02 10:28:04 +0800

    gumagana radio button dito

--- admin/editstaff.php
+++ admin/editstaff.php
@@ -70,7 +70,7 @@
 ?>
 <body>
     <?php
-        require_once('../include/header.admin.php')
+        require_once('../include/header-admin.php')
     ?>
     <main>
         <div class="container-fluid">
