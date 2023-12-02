6237703953102450f64b9899aa53984530780bfd
Sunshine Casido <qb202100057@wmsu.edu.ph>
2023-12-02 16:31:46 +0800

    fghjk

--- admin/addappointments.php
+++ admin/addappointments.php
@@ -1,6 +1,10 @@
 
 <?php
-   
+    session_start();
+    if (!isset($_SESSION['user']) || $_SESSION['user'] != 'employee'){
+        header('location: ./index.php');
+    }
+
     require_once '../classes/services.class.php';
     require_once '../classes/vets.class.php';
     require_once '../classes/client.class.php';
@@ -15,16 +19,7 @@
     $vets = new Vets();
     $vetsArray = $vets->show();
 
-   //resume session here to fetch session values
-   // session_start();
-   // /*
-   //     if user is not login then redirect to login page,
-   //     this is to prevent users from accessing pages that requires
-   //     authentication such as the dashboard
-   // */
-   // if (!isset($_SESSION['user']) || $_SESSION['user'] != 'staff'){
-   //     header('location: ./index.php');
-   // }
+  
 
   if(isset($_POST['save'])){
         $appt = new Appointment(); 
