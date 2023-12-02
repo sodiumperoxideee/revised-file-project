c67966fad96a78cba470c7e199a49518aa198599
Sunshine Casido <qb202100057@wmsu.edu.ph>
2023-12-02 17:14:54 +0800

    rrraaaaaaaahhhhh

--- admin/appointments.php
+++ admin/appointments.php
@@ -1,9 +1,9 @@
 <?php
-    // session_start();
+    session_start();
 
-    // if (!isset($_SESSION['user']) || $_SESSION['user'] != 'staff'){
-    //     header('location: index.php');
-    // }
+    if (!isset($_SESSION['user']) || $_SESSION['user'] != 'staff'){
+        header('location: index.php');
+    }
 ?>
 
 <!DOCTYPE html>
