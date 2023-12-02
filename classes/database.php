5170b4f2224594fb71290bacf43701e253cde2a4
Sunshine Casido <qb202100057@wmsu.edu.ph>
2023-12-02 19:42:14 +0800

    ampot

--- classes/database.php
+++ classes/database.php
@@ -4,7 +4,7 @@ class Database{
     private $host = 'localhost';
     private $username = 'root';
     private $password = '';
-    private $database = 'spicypizzaphp';
+    private $database = 'vetclinic';
     protected $connection;
 
     function connect(){
