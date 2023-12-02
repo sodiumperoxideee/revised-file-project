0be59b2dec490cb4b7fdf7c56a2021a6f2ad2527
chlorioineychee <makseyndelacruz@gmail.com>
2023-12-02 11:57:51 +0800

    overview change except for icons

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,10 +1,4 @@
-<?php
-    session_start();
 
-    if(!isset($_SESSION['user']) || (isset($_SESSION['user']) && $_SESSION['user'] != 'staff')){
-        header('location: index.php');
-    }
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +24,16 @@
                         <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
                             <div class="card admin-rounded">
                                 <div class="card-body">
-                                    <h5 class="card-title">Pending Orders</h5>
+                                    <h5 class="card-title">New Appointments</h5>
+                                    <p class="card-text"><i class="fa fa-users"></i> 1,000</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Statistic Card 1.1 -->
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
+                            <div class="card admin-rounded">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pending Confirmations</h5>
                                     <p class="card-text"><i class="fa fa-users"></i> 1,000</p>
                                 </div>
                             </div>
@@ -39,7 +42,7 @@
                         <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
                             <div class="card admin-rounded">
                                 <div class="card-body">
-                                    <h5 class="card-title">Total Orders</h5>
+                                    <h5 class="card-title">Finished Appointments</h5>
                                     <p class="card-text"><i class="fa fa-shopping-cart"></i> 500</p>
                                 </div>
                             </div>
@@ -48,20 +51,20 @@
                         <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
                             <div class="card admin-rounded">
                                 <div class="card-body">
-                                    <h5 class="card-title">Revenue</h5>
+                                    <h5 class="card-title">Services</h5>
                                     <p class="card-text"><i class="fa fa-money" aria-hidden="true"></i> &#8369;10,000</p>
                                 </div>
                             </div>
                         </div>
                         <!-- Statistic Card 4 -->
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
+                        <!-- <div class="col-12 col-sm-6 col-md-6 col-lg-3 pb-2 pb-lg-0">
                             <div class="card admin-rounded">
                                 <div class="card-body">
                                     <h5 class="card-title">Products</h5>
                                     <p class="card-text"><i class="fa fa-cubes"></i> 200</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <h2 class="h3 brand-color">New Orders</h2>
                     <div class="table-responsive">
